217521b07002f4f062e08110332d15ce9fe804ea
Fei Li <Philip@Feis-MacBook-Air.local>
2015-09-24 12:17:37 -0500

    test1

---
+++ practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  practice
+//
+//  Created by Fei Li on 9/23/15.
+//  Copyright (c) 2015 Fei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice
+//
+//  Created by Fei Li on 9/23/15.
+//  Copyright (c) 2015 Fei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
